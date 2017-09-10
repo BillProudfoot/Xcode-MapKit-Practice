03832b47bfb4418e369f9dd9c3ab853b7029ea17
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-09-10 22:20:55 +0100

    Initial Commit

---
+++ Map ProjectTests/Map_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Map_ProjectTests.swift
+//  Map ProjectTests
+//
+//  Created by Bill Proudfoot on 10/09/2017.
+//  Copyright © 2017 Bill Proudfoot. All rights reserved.
+//
+
+import XCTest
+@testable import Map_Project
+
+class Map_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
